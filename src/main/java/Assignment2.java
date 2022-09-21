75b39a36ea49f1367159c825f8c771497d5e3dd6
Tatsiana Auchynnik <tatsiana.auchynnik@gmail.com>
2022-09-21 15:37:08 -0700

    Formatted code

--- src/main/java/Assignment2.java
+++ src/main/java/Assignment2.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 
 /**
  * Assignment #2: Refactor Java code
@@ -11,25 +10,26 @@ import java.util.Arrays;
  * Please, review his implementation, see if there is a room for improvement and refactor it.
  */
 public class Assignment2 {
-    /**
-     * There is no requirement to capitalise resulting letters.
-     * @param name a string that includes from 0 to N words
-     * @return a string that represents the first letter from each word in the name followed by "."
-     */
-    public static String convertNameToInitials(String name) {
-        if(name == null) {
-          return null;
-        }
-        if(name.equals("")) {
-           return "";
-        }
-        StringBuilder initials = new StringBuilder();
-        String[] array = name.split(" ");
-        char initial;
-        for (String s : array) {
-            initial = s.charAt(0);
-            initials.append(initial).append(".");
-        }
-        return initials.toString();
+  /**
+   * There is no requirement to capitalise resulting letters.
+   *
+   * @param name a string that includes from 0 to N words
+   * @return a string that represents the first letter from each word in the name followed by "."
+   */
+  public static String convertNameToInitials(String name) {
+    if (name == null) {
+      return null;
     }
+    if (name.equals("")) {
+      return "";
+    }
+    StringBuilder initials = new StringBuilder();
+    String[] array = name.split(" ");
+    char initial;
+    for (String s : array) {
+      initial = s.charAt(0);
+      initials.append(initial).append(".");
+    }
+    return initials.toString();
+  }
 }
